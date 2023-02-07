5b0b3422e85bc08baa4c9a3fa6c2981ef0aa19e2
whalewalker <mongodbms@gmail.com>
2023-02-07 07:57:41 +0100

    Added open  API definition annotation to allow https calls

--- src/main/java/com/thullo/ThulloApplication.java
+++ src/main/java/com/thullo/ThulloApplication.java
@@ -1,11 +1,12 @@
 package com.thullo;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
-@EnableJpaRepositories(basePackages = "com.thullo.data.repository")
+@OpenAPIDefinition(servers = {@Server(url = "/", description = "Default server URL")})
 public class ThulloApplication {
     public static void main(String[] args) {
         SpringApplication.run(ThulloApplication.class, args);
