723bb05cd1a11ceb2a99873f3afd105279c8b328
whalewalker <mongodbms@gmail.com>
2023-02-07 08:56:19 +0100

    Added handler for 403 error

--- src/main/java/com/thullo/web/exception/CustomControllerAdvice.java
+++ src/main/java/com/thullo/web/exception/CustomControllerAdvice.java
@@ -1,15 +1,18 @@
 package com.thullo.web.exception;
 
 import com.thullo.web.payload.response.ErrorResponse;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.client.HttpStatusCodeException;
 import org.springframework.web.multipart.MaxUploadSizeExceededException;
 
 import java.time.LocalDateTime;
 
 @ControllerAdvice
+@Slf4j
 public class CustomControllerAdvice {
 
     @ExceptionHandler(MaxUploadSizeExceededException.class)
@@ -19,5 +22,17 @@ public class CustomControllerAdvice {
         error.setMessage("File size exceeded maximum limit of 1 MB");
         error.setTimestamp(LocalDateTime.now());
         return new ResponseEntity<>(error, HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(HttpStatusCodeException.class)
+    public ResponseEntity<ErrorResponse> handleHttpStatusCodeException(HttpStatusCodeException ex) {
+        HttpStatus statusCode = ex.getStatusCode();
+        if (statusCode == HttpStatus.FORBIDDEN) {
+            log.info("Error occurred ==> {}", (Object) ex.getStackTrace());
+            ErrorResponse errorResponse = new ErrorResponse( statusCode.value(), ex.getMessage());
+            log.info("Error response ==> {}", errorResponse);
+            return new ResponseEntity<>(errorResponse, statusCode);
+        }
+        return new ResponseEntity<>(ex.getStatusCode());
     }
 }
--- src/main/java/com/thullo/web/payload/response/ErrorResponse.java
+++ src/main/java/com/thullo/web/payload/response/ErrorResponse.java
@@ -1,12 +1,24 @@
 package com.thullo.web.payload.response;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 
 import java.time.LocalDateTime;
 
 @Data
+@AllArgsConstructor
 public class ErrorResponse {
     private int status;
     private String message;
     private LocalDateTime timestamp;
+
+    public ErrorResponse() {
+        timestamp = LocalDateTime.now();
+    }
+
+    public ErrorResponse(int status, String message) {
+        this.status = status;
+        this.message = message;
+        timestamp = LocalDateTime.now();
+    }
 }
