fbab412513b32eccc57546fa7efaebde2ab6deef
whalewalker <mongodbms@gmail.com>
2023-02-13 01:01:27 +0100

    fix cors issues on put request type

--- src/main/java/com/thullo/config/WebMvcConfig.java
+++ src/main/java/com/thullo/config/WebMvcConfig.java
@@ -17,7 +17,8 @@ public class WebMvcConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins(allowedOrigins);
+                .allowedOrigins(allowedOrigins)
+                .allowedMethods("HEAD", "GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS");
     }
 
 }
