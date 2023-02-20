4299b4dd4027f0a76d02869c6b995dd227d3e61f
whalewalker <mongodbms@gmail.com>
2023-02-20 21:13:50 +0100

    Added cache to label implementation

--- src/main/java/com/thullo/data/repository/LabelRepository.java
+++ src/main/java/com/thullo/data/repository/LabelRepository.java
@@ -1,6 +1,8 @@
 package com.thullo.data.repository;
 
 import com.thullo.data.model.Label;
+import org.springframework.cache.annotation.CachePut;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.lang.NonNull;
@@ -11,4 +13,10 @@ public interface LabelRepository extends JpaRepository<Label, Long> {
     @Query("select l from Label l where upper(l.name) = upper(?1)")
     Optional<Label> findByName(@NonNull String name);
 
+    @Override
+    @Cacheable(key = "boardId", value = "labels")
+    Optional<Label> findById(Long boardId);
+
+    @CachePut(key = "#result.id", value = "labels")
+    <S extends Label> S save(S label);
 }
