3b4affe10332eb77da6a47e85cd6f054f0e9187a
whalewalker <mongodbms@gmail.com>
2023-03-01 15:27:52 +0100

    Added websocket to project for full duplex request

---
+++ src/main/java/com/thullo/config/WebSocketConfig.java
@@ -0,0 +1,34 @@
+package com.thullo.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+
+@Configuration
+@EnableWebSocketMessageBroker
+public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+
+    /**
+     * Register STOMP endpoints mapping each to a specific URL and (optionally)
+     * enabling and configuring SockJS fallback options.
+     *
+     * @param registry
+     */
+    @Override
+    public void registerStompEndpoints(StompEndpointRegistry registry) {
+        registry.addEndpoint("/ws").setAllowedOriginPatterns("*").withSockJS();
+    }
+
+    /**
+     * Configure message broker options.
+     *
+     * @param registry
+     */
+    @Override
+    public void configureMessageBroker(MessageBrokerRegistry registry) {
+        registry.setApplicationDestinationPrefixes("/api/v1/thullo/comments");
+        registry.enableSimpleBroker("/chatroom");
+    }
+}
