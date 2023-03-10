1059aedc96ee21f953318a5eb90c5ceb6dc1ced2
whalewalker <mongodbms@gmail.com>
2023-03-10 22:06:22 +0100

    Added id to comment response

--- src/main/java/com/thullo/web/payload/response/CommentResponse.java
+++ src/main/java/com/thullo/web/payload/response/CommentResponse.java
@@ -6,6 +6,7 @@ import java.time.LocalDateTime;
 
 @Data
 public class CommentResponse {
+    private Long id;
     private String message;
     private String createdBy;
     private String imageUrl;
